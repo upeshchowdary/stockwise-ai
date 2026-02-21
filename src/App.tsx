8cf05223798079f9833c82452846b33d86526328
upeshchowdary <upeshchowdary.k@gmail.com>
2026-02-21 22:57:30 +0530

    fix: React Router basename for GitHub Pages + 404 SPA redirect

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <InstallPrompt />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/tracker" element={<Index />} />
